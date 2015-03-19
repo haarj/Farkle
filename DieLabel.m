412001c872fe99d0046aaefde7f2b2d5cc8eb65e
Justin Haar <justin.haar22@gmail.com>
2015-03-19 14:41:56 -0700

    Init Commit

---
+++ DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Justin Haar on 3/19/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

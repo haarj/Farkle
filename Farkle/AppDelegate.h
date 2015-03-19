6e9c4ccfac802bb731139e724caefc7045b5343d
Justin Haar <justin.haar22@gmail.com>
2015-03-19 14:40:04 -0700

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Justin Haar on 3/19/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Justin Haar on 3/19/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
